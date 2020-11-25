5dd4f6c50632349b0e90f80b697e892da98504c8
ZachJHall <zachjohnhall@gmail>
2020-11-24 23:02:19 -0600

    Added google analytics to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
-}
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      }
+    },
+],
+};
