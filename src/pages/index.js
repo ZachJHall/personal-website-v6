c8055cd27556d300b235b475bd490cc7ad3983e4
ZachJHall <zachjohnhall@gmail>
2020-12-13 02:48:36 -0600

    Added Suspense to test loading speed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React from "react"
+import React, {Suspense} from "react"
 import styled from 'styled-components'
 import './index.css'
 
 
 import Header from '../components/header'
 import Hero from '../components/hero'
-import Projects from '../components/projects'
+// import Projects from '../components/projects'
 
+const Projects = React.lazy(() => import ('../components/projects'));
 
 
 const ContainerDiv = styled.div`
@@ -18,7 +19,9 @@ export default function Home() {
     <ContainerDiv>
       <Header />
       <Hero />
-      <Projects />
+      <Suspense fallback ={<div>Loading...</div>}>
+        <Projects />
+      </Suspense>
     </ContainerDiv>
   )
 }
