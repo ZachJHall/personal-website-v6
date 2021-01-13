7ee9e267eb9d6a5ae845a47fc4295c8bb64deaf5
ZachJHall <zachjohnhall@gmail>
2021-01-12 23:17:51 -0600

    Created blogAttention component

---
+++ src/components/blogAttention.js
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background: blue;
+    height: 20vh;
+    color: white;
+
+    font-size: 1.5rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    flex-wrap: wrap;
+`
+const BlogAttention = () =>(
+   <Container>
+       <span>A little of what I have wrote:</span>
+       <span>Here</span>
+   </Container>
+)
+
+export default BlogAttention
