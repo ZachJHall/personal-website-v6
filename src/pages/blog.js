23a1b7a6b4362391acf67b79df1239e67bd59727
ZachJHall <zachjohnhall@gmail>
2021-01-12 23:19:41 -0600

    Created Blog Page

---
+++ src/pages/blog.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from 'styled-components'
+import './index.css'
+
+
+import Header from '../components/header'
+
+const Container = styled.div`
+
+`
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      
+    </Container>
+  )
+}
